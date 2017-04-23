c1454d64ff58a7ca8ee47c476ead184dd4f21e71
mbedoya <mauricio.bedoya@gmail.com>
2017-04-23 09:26:15 -0500

    Console Samples

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Console
+using Extensions;
+
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string displayFormat = "Original word: {0}\r\nPascal word: {1} \r\n";
+
+            string text1 = "hello";
+            Console.WriteLine(String.Format(displayFormat, text1, text1.ToPascal()));
+
+            string text2 = "hello world";
+            Console.WriteLine(String.Format(displayFormat, text2, text2.ToPascal()));
+
+            string text3 = " world";
+            Console.WriteLine(String.Format(displayFormat, text3, text3.ToPascal()));
+
+            Console.ReadLine();
         }
     }
 }
