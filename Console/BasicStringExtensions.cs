805a598e814b81cac8b1382e2f2a15a8a0ad81ac
mbedoya <mauricio.bedoya@gmail.com>
2017-04-23 09:18:17 -0500

    Initial Commit

---
+++ Console/BasicStringExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console
+{
+    public static class BasicStringExtensions
+    {
+        static string whiteSpace = " ";
+        static int notFoundIndexValue = -1;
+
+        public static string ToPascal(this String str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+
+            string upperLetter = string.Empty;
+            int firstLetterIndex = FindFirstLetterIndex(str);
+            if (firstLetterIndex == notFoundIndexValue)
+            {
+                return str;
+            }
+            upperLetter = str.Substring(0, firstLetterIndex + 1).ToUpper();
+
+            string unchangedStringPart = String.Empty;
+            if (str.Length > firstLetterIndex + 1)
+            {
+                unchangedStringPart = str.Substring(firstLetterIndex + 1, str.Length - firstLetterIndex - 1);
+            }
+
+            return upperLetter + unchangedStringPart;
+        }
+
+        private static int FindFirstLetterIndex(string str)
+        {
+            int index = notFoundIndexValue;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Substring(i, 1) != whiteSpace)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ UnitTest/BasicStringExtensionsTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Console;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class BasicStringExtensionsTest
+    {
+        [TestMethod]
+        public void ToPascal_LineIsEmpty_EmptyResult()
+        {
+            string text = string.Empty;
+            text = text.ToPascal();
+            Assert.AreEqual(string.Empty, text);
+        }
+
+        [TestMethod]
+        public void ToPascal_LineWithBlankSpaces_ReturnedValueIsWhatSent()
+        {
+            string text = "  ";
+            string originalValue = text;
+            text = text.ToPascal();
+            Assert.AreEqual(originalValue, text);
+        }
+
+        [TestMethod]
+        public void ToPascal_OneCharSent_CharConvertedToPascal()
+        {
+            string text = "h";
+            text = text.ToPascal();
+            Assert.AreEqual("H", text);
+        }
+
+        [TestMethod]
+        public void ToPascal_OneWordSent_WordConvertedToPascal()
+        {
+            string text = "hello";
+            text = text.ToPascal();
+            Assert.AreEqual("Hello", text);
+        }
+
+        [TestMethod]
+        public void ToPascal_StringStartsWithBlankSpaces_StringConvertedToPascal()
+        {
+            string text = "   hello";
+            text = text.ToPascal();
+            Assert.AreEqual("   Hello", text);
+        }
+
+        [TestMethod]
+        public void ToPascal_StringStartsWithBlankSpacesInMiddle_StringConvertedToPascal()
+        {
+            string text = "hello world";
+            text = text.ToPascal();
+            Assert.AreEqual("Hello world", text);
+        }
+
+    }
+}
